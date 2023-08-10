119ff50bcb076a628ee6bb36c4908acb9cc570db
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-10 13:59:23 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
 * main - Entry point
 *
@@ -8,11 +7,9 @@
 *
 * Return: 1 (not success)
 */
-
 int main(void)
 {
-  char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-  write(1, quo, 59);
-  return (1);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n;
+write(1, quo, 59);
+return (1);
 }
