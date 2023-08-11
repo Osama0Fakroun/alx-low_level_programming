8083ae3d642d9ed763cb3a75a04c50b6ee77d3df
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-11 10:33:27 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Descriotion: A C program that prints with printf function.
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-char ch = 'a';
-char CH = 'A';
-while (ch <= 'z')
-{
-putchar(ch);
-	ch++;
- }
-while (CH <= 'Z')
-  {
-putchar(CH);
-CH++;
-}
-putchar('\n');
-return (0);
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
 }
