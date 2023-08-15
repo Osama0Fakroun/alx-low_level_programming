6169484df43235a188d36e26c714b350b1f1abeb
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 00:36:07 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - check the code.
+* _islower - function to check if
+* character is lower
+*
+*@c: check input of functtiom
+*
+* Return: Always 1 if `c` is lower
+* othor wise always 0 (Success)
+*/
+int_islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+return (0);
+}
