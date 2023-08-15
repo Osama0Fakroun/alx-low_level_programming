44611d0d69c0f809cb0a3eef3b365c9a3692dd81
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 15:09:26 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* fibonacci - im tired
+*description: go sleep
+*
+*/
+int main(void)
+{
+unsigned long fib1 = 0, fib2 = 1, sum;
+float total_sum;
+while (1)
+{
+sum = fib1 + fib2;
+if (sum > 4000000)
+break;
+if ((sum %2) == 0)
+total_sum += sum;
+fib1 = fib2;
+fib2 = sum;
+}
+printf("%.0f\n", total_sum);
+return (0);
+}
