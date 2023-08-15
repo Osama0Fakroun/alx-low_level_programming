fce351f123cf653729cd39601f263e7e54a91f90
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 13:42:48 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 * print_alphabet_x10 - function that will print alphabet 10 times
 *
 */
-viod print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 int line, ch;
 for (line = 0; line <= 9; line++)
