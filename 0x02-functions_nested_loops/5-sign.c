65473b2a6e566fec8dea2ac10f6196e39ed56ac2
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 13:56:55 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
 * print_sign - print + if n >0
 * 0 if n = 0
 *@n: input if < 0
-* @c: takes integer type input for function
+* takes integer type input for function
 * Return: 1 if + , 0 if 0 and -1 if -.
 */
 int print_sign(int n)
