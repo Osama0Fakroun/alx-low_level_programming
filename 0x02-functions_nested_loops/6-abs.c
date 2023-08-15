11665c49866f023910d5ed47ebc546a03c0d232d
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 01:10:10 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - check the code
+* absolute function
+* @n: takes in integer type input for function
+* Return: Always 0.
+*/
+in _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
