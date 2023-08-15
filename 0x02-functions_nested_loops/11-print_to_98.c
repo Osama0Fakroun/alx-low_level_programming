25a6903361e95a2d3f3154de4c6f59b3474f461b
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 12:25:21 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*print_to_98
+*print_to_98 print n to 98 count
 * print n to 98 counts
 *Numbers must be separated by a comma, followed by a space
 *Numbers should be printed in order
 *The first printed number should be the number passed to your function
 * The last printed number should be 98
 *You are allowed to use the standard library
-* Return: Always 0.
+* @n: input
 */
 void print_to_98(int n)
 {
