09315b1c2e2ae5dac24336da5c3567313c51963c
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 14:50:33 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,12 +24,12 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar((prod / 10) % 10 +48)
-} else if (prod <= 99 && prod >= 10);
+_putchar((prod / 10) % 10 +48);
+} else if (prod <= 99 && prod >= 10)
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
 }
-_putchar(/n);
+_putchar('\n');
 }
 }
 }
