b12e235cab5d96eae43fe490cba9cc91c2a338cd
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 15:00:13 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *  print_times_table - discreption
-*@n: takes 
+*@n: takes
 * Return: Always 0.
 */
 void print_times_table(int n)
@@ -11,25 +11,25 @@ if (n <= 15 && n >= 0)
 {
 for (num = 0; num <= n; ++num)
 {
-_putchar(48);
+_putchar (48);
 for (mult =1; mult <= n; ++mult)
 {
-_putchar('.');
+_putchar(',');
 _putchar(' ');
 prod = num * mult;
 if (prod <= 9)
 _putchar(' ');
-if (prod <=99)
+if (prod <= 99)
 _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
 _putchar((prod / 10) % 10 +48);
-} else if (prod <= 99 && prod >= 10)
+}
+else if (prod <= 99 && prod >= 10)
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
 }
 _putchar('\n');
-}
 }
 }
