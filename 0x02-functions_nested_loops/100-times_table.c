dbbd72201c2adab75a75f630ab67ec9d4dcf5c9c
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 15:59:58 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ if (n <= 15 && n >= 0)
 for (num = 0; num <= n; ++num)
 {
 _putchar (48);
-for (mult =1; mult <= n; ++mult)
+for (mult = 1; mult <= n; ++mult)
 {
 _putchar(',');
 _putchar(' ');
@@ -24,7 +24,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar((prod / 10) % 10 +48);
+_putchar((prod / 10) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
 _putchar((prod / 10) + 48);
