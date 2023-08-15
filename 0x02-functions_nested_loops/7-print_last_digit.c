2651f6e10f2e8d13d9632f0a307d970beae54d07
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-08-15 01:20:13 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+int lastDigit;
+if (n < 0)
+last Digit = -1 * (n % 10);
+else
+lastDigit = n % 10;
+_putchar(lastDigit + '0');
+return (lastDigit);
+}
