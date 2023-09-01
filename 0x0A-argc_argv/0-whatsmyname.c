bfd61153a55a8ff53432456b196aea94cb97243a
Osama0Fakroun <fakrouno@hotmail.cpm>
2023-09-01 12:09:49 -0700

    0x0A C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
-#include <stdio.h>#include main.h/** * main - prints the name of the program * @argc: number of arguments * @argv: array of arguments * * Return: Always 0 (Success) */int main(int argc __attribute__((unused)), char *argv[]){        printf(%sn, *argv);        return (0);}
+#include <stdio.h>
+#include main.h
+/**
+* main - prints the name of the program
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf(%sn, *argv);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,5 @@
-#ifndef MAIN_H#define MAIN_Hint _putchar(char c);int _atoi(char *s);#endif
+#ifndef MAIN_H
+#define MAIN_Hint
+_putchar(char c);
+int _atoi(char *s);
+#endif
